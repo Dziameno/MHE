703a0ca8e1e703001cf9a96d11c4a1f6fb07e928
Tomasz Woźny <zuo@MacBook-Pro-Tomasz.local>
2022-11-07 11:30:45 +0100

    repo fix

--- LAB02/main.cpp
+++ LAB02/main.cpp
@@ -3,37 +3,47 @@
 
 struct bin_t {
     int weight_list_size;
-    int capacity; // pojemność pojemnika
+    int capacity;
     std::vector<int> weight;
     std::vector<std::string> bin_sum;
 };
 
-/**
- * -1 values are in "bag"
- */
-
 std::ostream &operator<<(std::ostream &o, const bin_t &bin) {
     using namespace std;
     o << "weights: ";
-    for (int x = 0; x < bin.weight.size(); x++) {
-        o << "*" << bin.weight[x] << "* ";
+    for (int x : bin.weight) {
+        o << "*" << x << "* ";
     }
     o << endl << "bins: ";
-    for (int y = 0; y < bin.bin_sum.size(); y++) {
-        o << "|" << bin.bin_sum[y]<< "| ";
+    for (const auto & y : bin.bin_sum) {
+        o << "|" << y<< "| ";
     }
     return o;
 }
 
+/*
+ *
+ */
+
 int main() {
     using namespace std;
-    std::vector<int> result_bins;
+    vector<int> result_bins;
+
     bin_t example_v1 = {
             7,
             10,
             {2, 5, 4, 7,1, 3, 8},
-            { "8+2", "7+3", "5+4+1" }
+            {"","","","",""}
     };
+
+    bin_t solution_v1 = {
+            7,
+            10,
+            {2, 5, 4, 7,1, 3, 8},
+            { "8+2", "7+3", "5+4+1", "","" }
+    };
+
     cout << example_v1 << endl;
+    cout << solution_v1 << endl;
     return 0;
 }
